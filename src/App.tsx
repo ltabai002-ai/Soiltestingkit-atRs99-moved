616cd5ba278e497a6c0ac47fcab372a86bcea6d0
kash2021-hobby <kashyapnandan2021@gmail.com>
2025-12-03 10:05:02 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import HeroSection from './components/HeroSection';
 import ConsequencesSection from './components/ConsequencesSection';
-import CTASection from './components/CTASection';
 import SolutionSection from './components/SolutionSection';
 import FinalOfferSection from './components/FinalOfferSection';
 import { createOrder } from './lib/supabase';
@@ -24,7 +23,6 @@ function App() {
     <div className="min-h-screen bg-white">
       <HeroSection />
       <ConsequencesSection />
-      <CTASection />
       <SolutionSection />
       <FinalOfferSection onSubmit={handleOrderSubmit} />
     </div>
